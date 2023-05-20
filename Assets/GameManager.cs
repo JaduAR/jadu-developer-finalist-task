a3a40ef5abf5117141d2c7063b0ad3799f963d95
uras <uras.oran.htc@gmail.com>
2023-05-20 15:14:07 -0400

    Added gradient UI bg Added sliding and fading tween for panels

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,4 +1,5 @@
 using Cinemachine;
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,16 +10,28 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] CameraManager CameraManager = CameraManager.Instance;
 
+    public float FadeDuration = 0.5f;
+
     public CinemachineVirtualCamera IdleCam;
     public CinemachineVirtualCamera SkinCloseUpCam;
     public CinemachineVirtualCamera HairCloseUpCam;
 
-    public GameObject CustomizationPanel;
+    public CanvasGroup CustomizationPanel;
+    
+    public RectTransform DoneButton;
+    public float DoneButtonInitial_Y = 125, DoneButtonFinal_Y = 0;
+
+    public RectTransform CustomizationSelectionPanel;
+    public float CustomizationSelectionPanel_Y = -500;
 
     public List<GameObject> CustomizationPanelTabs = new List<GameObject>();// Will contain all the below tabs
 
     public GameObject SkinCustomizationPanel;
+    public float SkinCustomizationPanel_Y = -250;
+
     public GameObject HairCustomizationPanel;
+    public float HairCustomizationPanel_Y = 100;
+
 
     void Awake()
     {
@@ -31,27 +44,39 @@ public class GameManager : MonoBehaviour
 
         CustomizationPanelTabs.Add(SkinCustomizationPanel);
         CustomizationPanelTabs.Add(HairCustomizationPanel);
+        SetIdleState();
     }
     public void SetIdleState()
     {
         CameraManager.SwitchCamera(IdleCam);
-        CustomizationPanel.SetActive(false);
+        DoneButton.DOAnchorPosY(DoneButtonInitial_Y, FadeDuration);
+        CustomizationSelectionPanel.DOAnchorPosY(CustomizationSelectionPanel_Y, FadeDuration).SetEase(Ease.Linear);
+        CustomizationPanel.DOFade(0, FadeDuration);
         DisableTabs();
     }
     public void SetSkinSelectState()
     {
         CameraManager.SwitchCamera(SkinCloseUpCam);
+        DoneButton.DOAnchorPosY(DoneButtonFinal_Y, FadeDuration);
+        CustomizationPanel.DOFade(1, FadeDuration);
+
         EnableTab(SkinCustomizationPanel);
+        CustomizationSelectionPanel.DOAnchorPosY(SkinCustomizationPanel_Y, FadeDuration).SetEase(Ease.Linear);
+        
     }
     public void SetHairSelectState()
     {
         CameraManager.SwitchCamera(HairCloseUpCam);
+        DoneButton.DOAnchorPosY(DoneButtonFinal_Y, FadeDuration);
+
+        CustomizationPanel.DOFade(1, FadeDuration);
         EnableTab(HairCustomizationPanel);
+        CustomizationSelectionPanel.DOAnchorPosY(HairCustomizationPanel_Y, FadeDuration).SetEase(Ease.Linear);
     }
 
     public void SetCustomizeState()
     {
-        CustomizationPanel.SetActive(true);
+        CustomizationPanel.gameObject.SetActive(true);
         SetSkinSelectState();
         if (Input.GetMouseButtonDown(0))
         {
