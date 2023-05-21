6a800c3b5a6844db4d03ed323aad134d88f8d5fc
uras <uras.oran.htc@gmail.com>
2023-05-21 12:42:34 -0400

    Signed waiver Fined tuned camera angles Fine tuned camera for landscape aspect ratio

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
-using static UnityEditor.Progress;
 using System.Linq;
 
 public class CameraManager : MonoBehaviour
